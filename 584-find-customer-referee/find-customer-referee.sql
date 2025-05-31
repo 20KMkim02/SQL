b1ba7a65a8a475ff12c6ea31a52e632cfa29ca9d
Yodsapat Setteemongkolchai <112478541+20KMkim02@users.noreply.github.com>
2025-05-31 22:58:33 +0700

    Time: 488 ms (57.11%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select name
-from Customer
-where referee_id != '2' or referee_id is null ;
+select name from Customer
+where referee_id != 2 or referee_id is null ;
+# ใช้ is null มา or เพิ่มในกรณีต้องการค่า null ด้วย
