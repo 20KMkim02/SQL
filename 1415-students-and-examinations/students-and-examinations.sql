72206575c07cda5c8fdeea41494098f4c256bc1c
Yodsapat Setteemongkolchai <112478541+20KMkim02@users.noreply.github.com>
2025-06-01 21:34:52 +0700

    Time: 1105 ms (19.19%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -2,7 +2,9 @@
 select st.student_id , st.student_name ,sj.subject_name , count(ex.subject_name) as attended_exams
 from Students as st
 cross join Subjects sj
+# cross join เป็นการทำ Cartesian product
 left join Examinations ex
 on st.student_id = ex.student_id and sj.subject_name = ex.subject_name
 group by st.student_id , st.student_name ,sj.subject_name
+# group by สามารถทำกับ column มากกว่า 1 ได้ (เหมือนอยากตรึงCollumnไหนไว้ก็เลือกอันนั้น)
 order by st.student_id , st.student_name ,sj.subject_name
